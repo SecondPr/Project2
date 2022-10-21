eb9abbab75a052cb7891285146d5a8ed81e78844
rkurbanov <ray.kurbanov@gmail.com>
2022-10-20 19:39:30 -0500

    updated db connection

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -11,5 +11,13 @@ router.get("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
+router.get("/login", async (req, res) => {
+  try {
+    res.render("login", { loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
